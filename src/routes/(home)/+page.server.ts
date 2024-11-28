dfe74b3c7b5b9b940c01eee601286c89aa97e2e7
Graeme Byrne <grmbrn89@gmail.com>
2024-11-28 10:54:07 +0100

    clean up +page.server.ts

--- src/routes/(home)/+page.server.ts
+++ src/routes/(home)/+page.server.ts
@@ -2,45 +2,40 @@ import features from '$lib/data/features';
 import { fetchMarkdownPosts } from '$lib/utils/index_posts';
 import type { Contributor } from '$lib/utils/types';
 
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+const BASE_URL = 'https://api.github.com';
+
+async function fetchWithAuth(url: string) {
+	const response = await fetch(url, {
+		headers: {
+			Authorization: `Bearer ${GITHUB_TOKEN}`
+		}
+	});
+
+	if (!response.ok) {
+		console.error(`Failed to fetch from ${url}:`, response.statusText);
+		throw new Error(`Failed to fetch from ${url}`);
+	}
+
+	return response.json();
+}
+
 export async function load() {
 	const allPosts = await fetchMarkdownPosts();
-
 	const posts = allPosts
 		.sort((a, b) => new Date(b.meta.date).getTime() - new Date(a.meta.date).getTime())
 		.slice(0, 6);
 
-	const orgReposURL = 'https://api.github.com/orgs/torrust/repos';
-	const baseURL = 'https://api.github.com/repos/torrust/';
-	const token = import.meta.env.VITE_GITHUB_TOKEN;
-
-	const repoResponse = await fetch(orgReposURL, {
-		headers: {
-			Authorization: `Bearer ${token}`
-		}
-	});
-
-	if (!repoResponse.ok) {
-		console.error('Failed to fetch repositories:', repoResponse.statusText);
-		throw new Error('Failed to fetch repositories');
-	}
-
-	const repos: { name: string }[] = await repoResponse.json();
-
-	const gitHubRepos: string[] = repos.map((repo: { name: string }) => repo.name);
-
-	const urls: string[] = gitHubRepos.map((repo) => `${baseURL}${repo}/contributors`);
+	const repos = await fetchWithAuth(`${BASE_URL}/orgs/torrust/repos`);
+	const gitHubRepos = repos.map((repo: { name: string }) => repo.name);
 
 	const contributorResponses = await Promise.all(
-		urls.map((url) =>
-			fetch(url, {
-				headers: {
-					Authorization: `Bearer ${token}`
-				}
-			}).then((res) => res.json())
+		gitHubRepos.map((repo: string) =>
+			fetchWithAuth(`${BASE_URL}/repos/torrust/${repo}/contributors`)
 		)
 	);
 
-	const allContributors: Contributor[] = [].concat(...contributorResponses);
+	const allContributors: Contributor[] = contributorResponses.flat();
 	const uniqueContributors = Array.from(
 		new Map(allContributors.map((contributor) => [contributor.login, contributor])).values()
 	);
